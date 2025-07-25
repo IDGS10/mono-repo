c836288391356de2dcef62b1deb990a5a0654c4d
Aldo Luna <aldoyamil23@hotmail.com>
2025-07-24 23:56:47 -0600

    Add: New route for apis of 'invitations'

--- backend/organizations/src/app.js
+++ backend/organizations/src/app.js
@@ -28,6 +28,7 @@ app.get('/health', (req, res) => {
 });
 
 app.use('/api/organizations', organizationRoutes);
+app.use('/api/invitations', invitationRoutes)
 app.use('/api/projects', projectRoutes);
 
 
