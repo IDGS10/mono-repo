163e48b845f58ac92c397c3ac53a71d66749c755
Timoteo Cruz <163707359+TimoteoCruz@users.noreply.github.com>
2025-08-16 15:58:37 -0600

    Devices timoteo 14072025 (#46)
    * Change the structure of the Dockerfiles in device-manager, and updateing the docker-compose in backend root
    
    * Stop tracking MQTT certificate

--- backend/device-manager/src/influx/kafkaInfluxConsumer.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=kafkaInfluxConsumer.d.ts.map
--- backend/device-manager/src/mqtt/handler/IDGS10-Pruebas-Sensores.d.ts
+++
@@ -1,2 +0,0 @@
-export default function (msg: string, topic?: string): Promise<void>;
-//# sourceMappingURL=IDGS10-Pruebas-Sensores.d.ts.map
